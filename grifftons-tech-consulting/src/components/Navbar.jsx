81482f6c95540a61b162f87642f5a2e2e5699161
tanewton0523 <59624833+tanewton0523@users.noreply.github.com>
2025-07-15 16:24:17 -0400

    added consulting in navbar

--- grifftons-tech-consulting/src/components/Navbar.jsx
+++ grifftons-tech-consulting/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import styles from './Navbar.module.css';
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>Griffton's Tech</div>
+      <div className={styles.logo}>Griffton's Tech Consulting</div>
       <ul className={styles.navLinks}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>

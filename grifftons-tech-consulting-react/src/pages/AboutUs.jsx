50089741a8df4786f35e2c62554ea5b0da1e042a
MsGem0523 <59624833+msgem0523@users.noreply.github.com>
2026-08-01 05:00:09 -0400

    fix: Correct section title and simplify resume link structure in AboutUs component

--- grifftons-tech-consulting-react/src/pages/AboutUs.jsx
+++ grifftons-tech-consulting-react/src/pages/AboutUs.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import TeMecha from './resume/TeMechaResume';
-// import Cameron from './resume/CamResume';
 
 function AboutUs() {
   return (
     <div>
-      <h2>Meet the Founders</h2>
+      <h2>Meet the Founder</h2>
         <section>
           <h3>TeMecha Griffin</h3>
           <p>TeMecha is a software engineer with a passion for developing innovative solutions that enhance user experience and drive business success.</p>
-          <button className="btn-primary">
-            <a
-              href="/resume/TeMecha"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn-primary inline-block"
-            >
-              <i className="fa-solid fa-file-lines mr-2"></i> View Resume
-            </a>
-          </button>
-
           
-        </section>
-        <section>
-          <h3>Cameron Griffin</h3>
-          
+          <a
+            href="/resume/TeMecha"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-primary inline-block"
+          >
+            <i className="fa-solid fa-file-lines mr-2"></i> View Resume
+          </a>
+                    
         </section>
     </div>
   );
